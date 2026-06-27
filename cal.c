bc87517b4f29e12a318d46a9ab33a474e99b80b2
ict2024130-ux <251212921+ict2024130-ux@users.noreply.github.com>
2026-06-28 01:51:24 +0530

    cal culate two numbers
    first number is num1
    
    second number is num2

---
+++ cal.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void){
+    int num1=0, num2=0, total=0;
+    printf("Enter number1 and number2: ");
+    scanf("%d %d",&num1 , &num2);
+    total= num1 +num2;
+    printf("your new learner...\n total is: %d",total);
+    return 0;
+}
+
